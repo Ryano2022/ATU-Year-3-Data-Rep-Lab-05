6249713215c5cf29c68a6152033f063da8f1eee2
Ryan Hogan <ryanoman2011@hotmail.com>
2023-10-27 15:06:44 +0100

    Initial commit.

---
+++ server.js
@@ -0,0 +1,95 @@
+const express = require('express')
+const app = express()
+const port = 4000; // Port to listen to.
+
+app.get(
+  '/test', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  }
+)
+
+app.get(
+  '/api/books', (req, res) => {
+    const data = [ 
+      { // Data for the API.
+        "books": [
+          {
+            "title": "Learn Git in a Month of Lunches",
+            "isbn": "1617292419",
+            "pageCount": 0,
+            "thumbnailUrl":
+              "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/umali.jpg",
+            "status": "MEAP",
+            "authors": ["Rick Umali"],
+            "categories": []
+          },
+          {
+            "title": "MongoDB in Action, Second Edition",
+            "isbn": "1617291609",
+            "pageCount": 0,
+            "thumbnailUrl":
+              "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/banker2.jpg",
+            "status": "MEAP",
+            "authors": [
+              "Kyle Banker",
+              "Peter Bakkum",
+              "Tim Hawkins",
+              "Shaun Verch",
+              "Douglas Garrett"
+            ],
+            "categories": []
+          },
+          {
+            "title": "Getting MEAN with Mongo, Express, Angular, and Node",
+            "isbn": "1617292036",
+            "pageCount": 0,
+            "thumbnailUrl":
+              "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/sholmes.jpg",
+            "status": "MEAP",
+            "authors": ["Simon Holmes"],
+            "categories": []
+          }
+        ]
+      }
+    ]
+
+    res.json(
+      {
+        martin_books:data, "Message": "Hello from server.js!"
+      }
+    );
+  }
+)
+
+// The slashes are URLs.
+app.get(
+  '/', (req, res) => {
+    res.send('Hello World!');
+  }
+)
+
+// Test
+app.get(
+  '/whatever', (req, res) => {
+    res.send('Good Bye!');
+  }
+)
+
+app.get(
+  '/datarep', (req, res) => {
+    res.send('Welcome to Data Representation & Querying');
+  }
+)
+
+// Anything can go where :name is on the address bar on the browser.
+app.get(
+  '/hello/:name', (req, res) => {
+    res.send('HELLO' + req.params.name);
+  }
+)
+
+app.listen(
+  port, () => {
+    console.log(`Example app listening on port ${port}`);
+  }
+)
